eb2cf581eb2b707e4341e9b0aa88efaa112eac08
CodeMnh <minhkhac1002@gmail.com>
2025-04-10 08:57:11 +0700

    install pugjs

--- index.ts
+++ index.ts
@@ -3,8 +3,13 @@ import express, { Express, Request, Response } from "express"
 const app: Express = express()
 const port: number = 3000
 
+// pug
+app.set('views', './views')
+app.set('view engine', 'pug')
+// end pug
+
 app.get('/topics', (req: Request, res: Response) => {
-  res.send('Trang chủ đề bài hat')
+  res.render("client/pages/topics/index.pug")
 })
 
 app.listen(port, () => {
